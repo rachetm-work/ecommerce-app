f907fc34687284348c728f6df61fa8da97418bc5
Rachet M <rachet.m@codeparva.com>
2024-11-24 21:50:50 +0530

    Added DB connection configuration

---
+++ app/framework/database/database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, URL
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import QueuePool
+
+from app.framework.settings import get_settings
+
+settings = get_settings()
+
+engine = create_engine(
+    url=URL.create(
+        drivername=settings.DB_DRIVER,
+        username=settings.DB_USERNAME,
+        password=settings.DB_PASSWORD,
+        host=settings.DB_HOST,
+        port=settings.DB_PORT,
+        database=settings.DB_NAME
+    ),
+    poolclass=QueuePool,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,
+    echo=False
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+async def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
