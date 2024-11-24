23f822cf3d68dba9f2bb2c7e1b5175f8311e2aa1
Rachet M <rachet.m@codeparva.com>
2024-11-24 23:24:56 +0530

    Added BaseCrudService class

---
+++ app/framework/services/__init__.py
@@ -0,0 +1,3 @@
+from .base_crud_service import BaseCrudService
+
+__all__ = ["BaseCrudService"]
---
+++ app/framework/services/base_crud_service.py
@@ -0,0 +1,15 @@
+class BaseCrudService:
+
+    def __init__(self, model, db_session):
+        self.model = model
+        self.db_session = db_session
+
+    def get_all(self):
+        return self.db_session.query(self.model).all()
+
+    def create(self, resource):
+        db_item = self.model(**resource.dict())
+        self.db_session.add(db_item)
+        self.db_session.commit()
+        self.db_session.refresh(db_item)
+        return db_item
