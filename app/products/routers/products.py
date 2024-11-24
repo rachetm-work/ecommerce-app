296558c35052250ea7f54fdebf117ace9d385f7e
Rachet M <rachet.m@codeparva.com>
2024-11-25 02:09:57 +0530

    Added return types

--- app/products/routers/products.py
+++ app/products/routers/products.py
@@ -11,14 +11,14 @@ router = APIRouter(prefix="/products", tags=["products"])
 
 
 @router.get("/")
-def get_all_products(db: Session = Depends(database.get_db)):
+def get_all_products(db: Session = Depends(database.get_db)) -> ApiSuccessResponse:
     products = ProductService(db_session=db).get_all_products()
     serializer = ApiModelSerializer(data_to_serialize=products)
     return ApiSuccessResponse(data=serializer)
 
 
 @router.post("/")
-def create_product(product: ProductCreateSchema, db: Session = Depends(database.get_db)):
+def create_product(product: ProductCreateSchema, db: Session = Depends(database.get_db)) -> ApiSuccessResponse:
     product = ProductService(db_session=db).create_product(product)
     serializer = ApiModelSerializer(data_to_serialize=product)
     return ApiSuccessResponse(data=serializer)
--- app/products/services/products_service.py
+++ app/products/services/products_service.py
@@ -1,4 +1,6 @@
-from app.framework.services.base_crud_service import BaseCrudService
+from typing import List
+
+from app.framework.services import BaseCrudService
 from app.products.models import Product
 
 
@@ -6,8 +8,8 @@ class ProductService(BaseCrudService):
     def __init__(self, db_session):
         super().__init__(model=Product, db_session=db_session)
 
-    def get_all_products(self):
+    def get_all_products(self) -> List[Product]:
         return super().get_all()
 
-    def create_product(self, product):
+    def create_product(self, product) -> Product:
         return super().create(product)
