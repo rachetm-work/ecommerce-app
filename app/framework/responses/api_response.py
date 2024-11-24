8305f7631c596a1bf94aeac5305038a0fcbca742
Rachet M <rachet.m@codeparva.com>
2024-11-25 00:50:20 +0530

    Added db model response serializer

--- app/framework/responses/api_response.py
+++ app/framework/responses/api_response.py
@@ -2,10 +2,17 @@ from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
 
 from app.framework.schemas import SuccessResponse, ErrorResponse
+from app.framework.serializers import ApiModelSerializer
 
 
 class ApiSuccessResponse(JSONResponse):
     def __init__(self, success=True, data=None, **kwargs):
+        if isinstance(data, ApiModelSerializer):
+            data = data.serialize()
+
+        if not isinstance(data, list):
+            data = [data]
+
         super().__init__(content=jsonable_encoder(SuccessResponse(success=success, data=data)), **kwargs)
 
 
---
+++ app/framework/serializers/model_serializer.py
@@ -0,0 +1,24 @@
+from app.framework.models import BaseModel
+
+
+class ApiModelSerializer:
+    """Serializer for db models"""
+
+    def __init__(self, data_to_serialize=None, **kwargs):
+        self.data_to_serialize = data_to_serialize
+
+    def serialize(self):
+        """Serialize models."""
+        if not isinstance(self.data_to_serialize, list):
+            self.data_to_serialize = [self.data_to_serialize]
+
+        result = []
+        for data in self.data_to_serialize:
+            if isinstance(data, BaseModel):
+                schema = data._schema
+                serialized_data = schema.model_validate(data)
+                result.append(serialized_data)
+            else:  # Anything that is not a BaseModel. Eg: dict, Pydantic model, etc.
+                result.append(data)
+
+        return result
