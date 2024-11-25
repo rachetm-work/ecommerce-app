9890a68486e2d049649ba274dd2a08fd139fe60a
Rachet M <rachet.m@codeparva.com>
2024-11-25 09:51:12 +0530

    Added order and product routers to base router

--- app/framework/routers/base.py
+++ app/framework/routers/base.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
 
 from app.framework.settings import get_settings
+from app.orders.routers import orders_router
+from app.products.routers import products_router
 
 settings = get_settings()
 
 base_router = APIRouter(prefix=settings.BASE_URL_PREFIX)
+
+base_router.include_router(products_router)
+base_router.include_router(orders_router)
--- app/orders/services/__init__.py
+++ app/orders/services/__init__.py
@@ -1,4 +1,3 @@
-from .order_comfirmation_service import OrderConfirmationService
 from .orders_service import OrderService
 
-__all__ = ["OrderService", "OrderConfirmationService"]
+__all__ = ["OrderService"]
