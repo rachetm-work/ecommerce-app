3a5df0ade7a4b079cd65134386af1aa0f6769ad8
Rachet M <rachet.m@codeparva.com>
2024-11-25 23:28:42 +0530

    Added deployment related files

---
+++ deployment/deploy.sh
@@ -0,0 +1,5 @@
+# Stop the existing containers
+docker-compose down
+
+# Run the Services
+docker-compose up -d --build
---
+++ deployment/init.sql
@@ -0,0 +1,4 @@
+-- Initial script to create the database and tables on docker container start
+
+CREATE DATABASE ecommerce;
+CREATE DATABASE ecommerce_test;
